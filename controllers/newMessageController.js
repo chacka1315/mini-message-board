2b9ae1d42e73bb482f0bf653b0f09d166ebb97a8
chacka1315 <chacka131@gmail.com>
2025-10-26 19:14:35 +0000

    Add a getDate function for messages dates.

--- controllers/newMessageController.js
+++ controllers/newMessageController.js
@@ -1,1 +1,27 @@
-export function newMsgController(req, res) {}
+import db from '../db.js';
+function handleGET(req, res) {
+  res.locals.title = 'New meaage';
+  res.render('pages/form');
+}
+
+function handlePOST(req, res) {
+  const { user_name, user_message } = req.body;
+  const data = {
+    user: user_name,
+    text: user_message,
+    added: getDate(),
+    id: crypto.randomUUID(),
+  };
+  db.addMessage(data);
+  res.redirect('/');
+}
+
+function getDate() {
+  const date = new Date().toISOString().split('T');
+  const day = date[0];
+  let time = date[1].split('.')[0];
+  time = time.slice(0, 5);
+  const result = `${day} ${time}`;
+  return result;
+}
+export default { get: handleGET, post: handlePOST };
