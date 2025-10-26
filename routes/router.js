9797642c4b206046cce37289b5dc14fa0382d7ea
chacka1315 <chacka131@gmail.com>
2025-10-26 15:56:43 +0000

    Change the name of the router.

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { newMsgController } from '../controllers/newMessageController.js';
+import newMessageController from '../controllers/newMessageController.js';
 import { indexController } from '../controllers/indexController.js';
+import { messageController } from '../controllers/messageController.js';
+import NotFoundError from '../errors/NotFoundError.js';
 
 const router = express.Router();
 
 router.get('/', indexController);
-router.get('/new', newMsgController);
-
+router.get('/messages/:messageId', messageController);
+router.get('/new', newMessageController.get);
+router.post('/new', newMessageController.post);
+router.use((req, res) => {
+  throw new NotFoundError('This Page does not exist!');
+});
 export default router;
